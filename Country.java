2bf957c39d15e31338457d684b04bf0cdcd242d7
Hari L <hariprasathlingaraj@gmail.com>
2022-07-06 18:36:27 +0530

    Adding country.java

---
+++ Country.java
@@ -0,0 +1,18 @@
+class Country
+{
+void revolve()
+{
+System.out.println("REVOLVE");
+}
+void comingSoon()
+{
+System.out.println("COMING SOON");
+}
+public static void main(String[] args)
+{
+	Country India =new Country();
+	India.revolve();
+	India.comingSoon();
+	
+}
+}
